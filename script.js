4ab1be3103c557b7f3136ba820acdb6ab25712be
Alexa Pierce <bondmd8@gmail.com>
2026-01-02 10:09:14 +0600

    Refactor offer rendering to include links
    Updated offer rendering logic to include links for each offer.

--- script.js
+++ script.js
@@ -1,67 +1,49 @@
-const smartLink = "https://fibredelivery.com/ncm9wbnq8?key=e199a7a403eb1ccd494e76f512ecc11a";
-
 const offers = [
-  {name:"AI Allure", img:"aiallure.jpg"},
-  {name:"AI Allure 2", img:"aiallure2.jpg"},
-  {name:"Aimour.ai", img:"Aimour.ai.jpg"},
-  {name:"Aimour.ai 2", img:"Aimour.ai2.jpg"},
-  {name:"BrainPill", img:"BrainPill.jpg"},
-  {name:"Candy.ai", img:"Candy.ai.jpg"},
-  {name:"Candy.ai 2", img:"Candy.ai2.jpg"},
-  {name:"DreamBF.ai", img:"DreamBF.ai.jpg"},
-  {name:"DreamGF.ai", img:"Dreamgf.ai.jpg"},
-  {name:"eHentai.ai", img:"eHentai.ai.jpg"},
-  {name:"Fanfinity", img:"Fanfinity.jpg"},
-  {name:"Fanfinity 2", img:"Fanfinity2.jpg"},
-  {name:"Fantasy.AI", img:"Fantasy.Ai.jpg"},
-  {name:"Get-Harder", img:"Get-Harder.jpg"},
-  {name:"Girlfriend GPT", img:"Girlfriend GPT.png"},
-  {name:"GoLove.ai", img:"golove.ai.jpg"},
-  {name:"GoLove.ai 2", img:"golove.ai.jpg"},
-  {name:"JOI", img:"Joi.jpg"},
-  {name:"JOI 2", img:"Joi2.jpg"},
-  {name:"JOI 3", img:"Joi3.jpg"},
-  {name:"JOI 4", img:"Joi4.jpg"},
-  {name:"JOI 5", img:"Joi5.jpg"},
-  {name:"JustSext", img:"JustSext.jpg"},
-  {name:"Kupid.ai", img:"Kupid.png"},
-  {name:"Lovescape", img:"Lovescape.jpg"},
-  {name:"Luvr.ai", img:"Luvr.Ai.jpg"},
-  {name:"MyLovely AI", img:"MyLovely Ai.jpg"},
-  {name:"OurDream.ai", img:"ourdream.ai.png"},
-  {name:"OurDream.ai 2", img:"ourdream.ai2.png"},
-  {name:"Pleasur.ai", img:"Pleasur.Ai.jpg"},
-  {name:"Pleasur.ai 2", img:"Pleasur.Ai2.jpg"},
-  {name:"PocketGirl AI", img:"Pocketgirl Ai.jpg"},
-  {name:"Secret Desires AI", img:"Secret Desires AI.jpg"},
-  {name:"Secret Desires AI 2", img:"Secret Desires AI2.jpg"},
-  {name:"Secrets.ai", img:"secret.jpg"},
-  {name:"Spicier", img:"Spicier.jpg"},
-  {name:"Swipey", img:"Swipey.jpg"},
-  {name:"Swipey 2", img:"Swipey2.jpg"},
-  {name:"TestRX", img:"TestRX.jpg"},
-  {name:"Testosterone Support", img:"Testosterone Support Innerbody Labs.jpg"},
-  {name:"Transgender AI", img:"Transgender.jpg"}
+  {img:"Aimour.ai.jpg",link:"https://t.crjmpx.com/40746/7961"},
+  {img:"Candy.ai.jpg",link:"https://t.crjmpx.com/40746/7962"},
+  {img:"DreamBF.ai.jpg",link:"https://t.crjmpx.com/40746/8023"},
+  {img:"Fanfinity.jpg",link:"https://t.crjmpx.com/40746/7986"},
+  {img:"Joi.jpg",link:"https://t.crjmpx.com/40746/7306"},
+  {img:"Kupid.png",link:"https://t.crjmpx.com/40746/6646"},
+  {img:"Lovescape.jpg",link:"https://t.crjmpx.com/40746/7793"},
+  {img:"Luvr.Ai.jpg",link:"https://t.crjmpx.com/40746/6559"},
+  {img:"Pocketgirl Ai.jpg",link:"https://t.crjmpx.com/40746/6523"},
+  {img:"Spicier.jpg",link:"https://t.crjmpx.com/40746/6558"},
+  {img:"Swipey.jpg",link:"https://t.crjmpx.com/40746/7722"},
+  {img:"Swipey2.jpg",link:"https://t.crjmpx.com/40746/7721"},
+  {img:"Fantasy.Ai.jpg",link:"https://t.crjmpx.com/40746/7511"},
+  {img:"Get-Harder.jpg",link:"https://t.crjmpx.com/40746/7795"},
+  {img:"JustSext.jpg",link:"https://t.crjmpx.com/40746/7477"},
+  {img:"Secret.jpg",link:"https://t.crjmpx.com/40746/7912"},
+  {img:"Secret2.jpg",link:"https://t.crjmpx.com/40746/7911"},
+  {img:"Ourdream.jpg",link:"https://t.crjmpx.com/40746/7761"},
+  {img:"TestRX.jpg",link:"https://t.crjmpx.com/40746/7875"},
+  {img:"SpicyAI.jpg",link:"https://t.crjmpx.com/40746/7946"},
+  {img:"BrainPill.jpg",link:"https://t.crjmpx.com/40746/6924"},
+  {img:"Pleasur.Ai.jpg",link:"https://t.crjmpx.com/40746/7887"},
+  {img:"Pleasur2.jpg",link:"https://t.crjmpx.com/40746/7944"},
+  {img:"DreamGF.jpg",link:"https://t.crjmpx.com/40746/10318"},
+  {img:"AIAllure.jpg",link:"https://t.crjmpx.com/40746/7710"},
+  {img:"AIAllure2.jpg",link:"https://t.crjmpx.com/40746/7709"},
+  {img:"Golove.jpg",link:"https://t.crjmpx.com/40746/7401"},
+  {img:"MyLovely.jpg",link:"https://t.crjmpx.com/40746/7757"},
+  {img:"Fantasy2.jpg",link:"https://t.crjmpx.com/40746/7927"},
+  {img:"DreamAI.jpg",link:"https://t.crjmpx.com/40746/7976"},
+  {img:"AIHot.jpg",link:"https://t.crjmpx.com/40746/7977"},
+  {img:"SecretX.jpg",link:"https://t.crjmpx.com/40746/10339"},
+  {img:"HotAI.jpg",link:"https://t.crjmpx.com/40746/7942"},
+  {img:"AIChat.jpg",link:"https://t.crjmpx.com/40746/10100"},
+  {img:"LoveBot.jpg",link:"https://t.crjmpx.com/40746/10219"},
+  {img:"FantasyPlus.jpg",link:"https://t.crjmpx.com/40746/7910"},
+  {img:"DreamTalk.jpg",link:"https://t.crjmpx.com/40746/7103"},
+  {img:"AIQueen.jpg",link:"https://t.crjmpx.com/40746/7933"}
 ];
 
-const grid = document.getElementById("offerGrid");
-
-function renderOffers(list){
-  grid.innerHTML = "";
-  list.forEach(o=>{
-    grid.innerHTML += `
-      <div class="offer">
-        <img src="images/${o.img}">
-        <h3>${o.name}</h3>
-        <a href="${smartLink}" target="_blank">Visit Now</a>
-      </div>
-    `;
-  });
-}
-
-renderOffers(offers);
-
-document.getElementById("searchInput").addEventListener("keyup", e=>{
-  const q = e.target.value.toLowerCase();
-  renderOffers(offers.filter(o=>o.name.toLowerCase().includes(q)));
+const box = document.getElementById("offers");
+offers.forEach(o=>{
+  box.innerHTML += `
+    <div class="offer">
+      <img src="images/${o.img}">
+      <a href="${o.link}" target="_blank">Visit Now</a>
+    </div>`;
 });
